82d7ba73a6e15a3b67f61ceb56faa2a089fa8e6a
Lavish Palia <lavishgarg1199@gmail.com>
2023-07-27 20:02:39 +0530

    added logic to render flatlist of posts on userfeed

--- App.tsx
+++ App.tsx
@@ -1,48 +1,12 @@
 import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
-import FeedPost from './src/components/FeedPost';
-
-const post = {
-  id: '1',
-  createdAt: '19 December 2021',
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus unde blanditiis. Eos fugiat dolorem ea fugit aut sapiente corrupti autem dolores deleniti architecto, omnis, amet unde dignissimos quam minima?',
-  user: {
-    id: '1',
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    username: 'vadimnotjustdev',
-    name: 'Vadim',
-  },
-  nofComments: 11,
-  nofLikes: 34,
-  comments: [
-    {
-      id: '1',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        id: 'u1',
-        username: 'vadimnotjustdev',
-        name: 'Vadim',
-      },
-    },
-    {
-      id: '2',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        id: 'u1',
-        username: 'vadimnotjustdev',
-        name: 'Vadim',
-      },
-    },
-  ],
-};
+import {View, StyleSheet} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 
 const App = () => {
   return (
-    <ScrollView style={styles.app}>
-      <FeedPost post={post} />
-    </ScrollView>
+    <View style={styles.app}>
+      <HomeScreen />
+    </View>
   );
 };
 
